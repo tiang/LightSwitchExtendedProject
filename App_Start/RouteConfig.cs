a34581f37d780b07e437f32c92edd67ca76ac97e
Dale Morrison <dale.morrison@live.com>
2013-10-18 22:31:13 -0700

    Initial Commit

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace LightSwitchApplication
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{*allsvc}", new { allsvc = @".*\.svc(/.*)?" });
+
+            routes.MapRoute(
+                name: "Home",
+                url: "",
+                defaults: new { controller = "Home", action = "index" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
+
---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+
+namespace LightSwitchApplication
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+             name: "DefaultRpc",
+             routeTemplate: "rpc/{controller}/{action}/{id}",
+             defaults: new { id = RouteParameter.Optional }
+             );
+        }
+    }
+}
